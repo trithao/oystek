4c70fcd39999c6ae9ea0bf3ee296b1705a4076e2
trithao.cao <trithao.cao@gmail.com>
2013-03-07 22:17:49 +1030

    process oystek log files

--- oystek_plot_01.py
+++ oystek_plot_01.py
@@ -1,6 +1,6 @@
 from matplotlib import *
 
-n-regsiters = 16
+n_registers = 16
 oyster_dim = numpy.zeros(n_registers)
 
 n_images = 4
@@ -25,22 +25,22 @@ def plot_oyster_images():
     pyplot.scatter(oyster_images[i].T[0], oyster_images[i].T[0])
     # wait for a CR then plot the next image
   # plot all images in one window
+  return
 
-
-def fetch_1_oyster_interactive:
+def fetch_1_oyster_interactive():
   # wait for and get oyster data from COM port
   # interactive / blocking I/O operation
   return oyster_dim, oyster_images, oyster_velocity
 
 def oystek_start(fname):
   fin, fout = oystek_init(fname)
-  while (1) # read data till EOF
-
+  while (1):  
+  # read data till EOF
   fin.close()
   fout.close()
   return
   
-def oystek_start_interactive:
+def oystek_start_interactive():
   return
 
 
